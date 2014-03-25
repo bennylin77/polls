f7dd317e224b2e5a9e1fc7c9e213a86fe9672c0e
shihweiyao <shihweiyao@gmail.com>
2014-03-25 22:10:33 +0800

    poll option change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,22 @@ module ApplicationHelper
   def host
     'http://rtes.funeasy.tw'
   end  
+  def userOptionBlank(hash={})
+    @poll = Poll.find(hash[:id])
+    user_option=nil
+      @poll.poll_options.each do |o|
+        if !UserOption.where(user_id: session[:user_id], poll_option_id: o.id).first.blank?
+          user_option=UserOption.where(user_id: session[:user_id], poll_option_id: o.id).first  
+        end
+      end
+      if user_option.blank?
+        return 1
+      else
+        if 3600-(Time.now-user_option.updated_at)<=0
+          return 2
+          else
+            return 3
+          end
+      end
+  end
 end
