1831bd244fffb23e3f48ae9327b5d323d4c57dc1
shihweiyao <shihweiyao@gmail.com>
2014-03-25 17:49:54 +0800

    tickets add

--- app/models/poll_option.rb
+++ app/models/poll_option.rb
@@ -1,10 +1,8 @@
 # encoding: UTF-8
 class PollOption < ActiveRecord::Base
   attr_accessible :title
-<<<<<<< HEAD
-  
-=======
+
   has_many :user_options
   has_many :user_option_histories
->>>>>>> a0a45fdd290dad68d8e60b13b74c6cbf831d630c
+
 end
