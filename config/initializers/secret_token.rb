1421e52509c0169c1e2b58c9e3a47b3e17838bf5
bennylin77 <bennylin77@gmail.com>
2014-03-27 13:52:55 +0800

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Polls::Application.config.secret_token = '9a46ca0ad95205c4913cb17c854894a9f19ba19c6f0d1b439f4fb440fcb86d0b4ec236fe3c3178bf236a0187fce88ea26c6bdcb9b3ec41468c6113e78080647a'
